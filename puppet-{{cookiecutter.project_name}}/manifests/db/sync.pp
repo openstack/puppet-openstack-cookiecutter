3e9ae8bafcdce007c36d68440e56527db7b4807e
Emilien Macchi <emilien@redhat.com>
2015-10-02 08:51:32 -0400

    db::sync: drop User dependency
    We want to get rid of User POSIX dependency, let's fix it here.
    
    Change-Id: Ic55b06b5bcaec5edd55faf190b2b2d326726d160

--- puppet-{{cookiecutter.project_name}}/manifests/db/sync.pp
+++ puppet-{{cookiecutter.project_name}}/manifests/db/sync.pp
@@ -17,7 +17,6 @@ class {{cookiecutter.project_name}}::db::sync(
     user        => '{{cookiecutter.project_name}}',
     refreshonly => true,
     subscribe   => [Package['{{cookiecutter.project_name}}'], {{cookiecutter.project_name|capitalize}}_config['database/connection']],
-    require     => User['{{cookiecutter.project_name}}'],
   }
 
   Exec['{{cookiecutter.project_name}}-manage db_sync'] ~> Service<| title == '{{cookiecutter.project_name}}' |>
