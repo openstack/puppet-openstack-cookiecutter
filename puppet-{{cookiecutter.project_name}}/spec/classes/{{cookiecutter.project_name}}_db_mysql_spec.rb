02696c4a71b84cbc08ab62db54c7822828a41400
Denis Egorenko <degorenko@mirantis.com>
2015-11-16 16:06:53 +0300

    Update template for db_mysql spec tests
    Changes:
    * add tests for case when params were overrided;
    * remove duplicate test for allowed_hosts param in string case;
    * use only ':' for handling parameter name in tests;
    
    Change-Id: I2360df771f786b036d0b425c4ff0648f99e353df

--- puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_db_mysql_spec.rb
+++ puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_db_mysql_spec.rb
@@ -21,42 +21,40 @@ describe '{{cookiecutter.project_name}}::db::mysql' do
 
   describe 'with only required params' do
     it { is_expected.to contain_openstacklib__db__mysql('{{cookiecutter.project_name}}').with(
-      'user'          => '{{cookiecutter.project_name}}',
-      'password_hash' => '*3DDF34A86854A312A8E2C65B506E21C91800D206',
-      'dbname'        => '{{cookiecutter.project_name}}',
-      'host'          => '127.0.0.1',
-      'charset'       => 'utf8',
+      :user           => '{{cookiecutter.project_name}}',
+      :password_hash  => '*3DDF34A86854A312A8E2C65B506E21C91800D206',
+      :dbname         => '{{cookiecutter.project_name}}',
+      :host           => '127.0.0.1',
+      :charset        => 'utf8',
       :collate        => 'utf8_general_ci',
     )}
   end
 
   describe "overriding allowed_hosts param to array" do
-    let :params do
-      {
-        :password       => '{{cookiecutter.project_name}}pass',
-        :allowed_hosts  => ['127.0.0.1','%']
-      }
-    end
+    before { params.merge!( :allowed_hosts  => ['127.0.0.1','%'] ) }
 
+    it { is_expected.to contain_openstacklib__db__mysql('{{cookiecutter.project_name}}').with(
+      :user           => '{{cookiecutter.project_name}}',
+      :password_hash  => '*3DDF34A86854A312A8E2C65B506E21C91800D206',
+      :dbname         => '{{cookiecutter.project_name}}',
+      :host           => '127.0.0.1',
+      :charset        => 'utf8',
+      :collate        => 'utf8_general_ci',
+      :allowed_hosts  => ['127.0.0.1','%']
+    )}
   end
   describe "overriding allowed_hosts param to string" do
-    let :params do
-      {
-        :password       => '{{cookiecutter.project_name}}pass2',
-        :allowed_hosts  => '192.168.1.1'
-      }
-    end
+    before { params.merge!( :allowed_hosts  => '192.168.1.1' ) }
 
-  end
-
-  describe "overriding allowed_hosts param equals to host param " do
-    let :params do
-      {
-        :password       => '{{cookiecutter.project_name}}pass2',
-        :allowed_hosts  => '127.0.0.1'
-      }
-    end
-
+    it { is_expected.to contain_openstacklib__db__mysql('{{cookiecutter.project_name}}').with(
+      :user           => '{{cookiecutter.project_name}}',
+      :password_hash  => '*3DDF34A86854A312A8E2C65B506E21C91800D206',
+      :dbname         => '{{cookiecutter.project_name}}',
+      :host           => '127.0.0.1',
+      :charset        => 'utf8',
+      :collate        => 'utf8_general_ci',
+      :allowed_hosts  => '192.168.1.1'
+    )}
   end
 
 end
