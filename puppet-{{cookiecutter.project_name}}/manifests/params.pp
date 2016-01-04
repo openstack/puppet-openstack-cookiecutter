5da2f888267a9e1554c3959ee9ac274b1779e23f
Alex Schultz <aschultz@mirantis.com>
2016-01-04 13:19:03 -0700

    Support of PyMySQL driver for MySQL backend
    This change adds the ability to use the python-pymsql library as a
    backend for MySQL connections.
    
    Change-Id: I6d33a6afb7c35c75093270ffefb333b974e04021
    Co-Authored-By: Ivan Berezovskiy <iberezovskiy@mirantis.com>

--- puppet-{{cookiecutter.project_name}}/manifests/params.pp
+++ puppet-{{cookiecutter.project_name}}/manifests/params.pp
@@ -5,9 +5,11 @@ class {{cookiecutter.project_name}}::params {
   case $::osfamily {
     'RedHat': {
       $sqlite_package_name  = undef
+      $pymysql_package_name = undef
     }
     'Debian': {
       $sqlite_package_name  = 'python-pysqlite2'
+      $pymysql_package_name = 'python-pymysql'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
