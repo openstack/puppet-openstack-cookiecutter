b3c367fe8af63bfa00a107970d392e6bedc52083
Mykyta Karpin <mkarpin@mirantis.com>
2017-04-07 13:44:31 +0300

    oslo log: check puppet resource instead of actual config in spec
    Change-Id: Iaf9151ddbad72bbf4b0dd091d88fee5d389db66c

--- puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_logging_spec.rb
+++ puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_logging_spec.rb
@@ -17,8 +17,7 @@ describe '{{cookiecutter.project_name}}::logging' do
       :publish_errors => true,
       :default_log_levels => {
         'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
-        'qpid' => 'WARN', 'sqlalchemy' => 'WARN', 'suds' => 'INFO',
-        'iso8601' => 'WARN',
+        'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
         'requests.packages.urllib3.connectionpool' => 'WARN' },
      :fatal_deprecations => true,
      :instance_format => '[instance: %(uuid)s] ',
@@ -28,7 +27,7 @@ describe '{{cookiecutter.project_name}}::logging' do
      :use_stderr => false,
      :syslog_log_facility => 'LOG_FOO',
      :log_dir => '/var/log',
-     :log_file => '/var/log/{{cookiecutter.project_name}}.log',
+     :log_file => '/var/tmp/{{cookiecutter.project_name}}_random.log',
      :debug => true,
     }
   end
@@ -57,62 +56,51 @@ describe '{{cookiecutter.project_name}}::logging' do
 
   shared_examples 'basic default logging settings' do
     it 'configures {{cookiecutter.project_name}} logging settings with default values' do
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/use_syslog').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/use_stderr').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/syslog_log_facility').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_dir').with(:value => '/var/log/{{cookiecutter.project_name}}')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_file').with(:value => '/var/log/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.log')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/debug').with(:value => '<SERVICE DEFAULT>')
+      is_expected.to contain_oslo__log('{{cookiecutter.project_name}}_config').with(
+        :use_syslog          => '<SERVICE DEFAULT>',
+        :use_stderr          => '<SERVICE DEFAULT>',
+        :syslog_log_facility => '<SERVICE DEFAULT>',
+        :log_dir             => '/var/log/{{cookiecutter.project_name}}',
+        :log_file            => '/var/log/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.log',
+        :debug               => '<SERVICE DEFAULT>',
+      )
     end
   end
 
   shared_examples 'basic non-default logging settings' do
     it 'configures {{cookiecutter.project_name}} logging settings with non-default values' do
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/use_syslog').with(:value => 'true')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/use_stderr').with(:value => 'false')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/syslog_log_facility').with(:value => 'LOG_FOO')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_dir').with(:value => '/var/log')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_file').with(:value => '/var/log/{{cookiecutter.project_name}}.log')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/debug').with(:value => 'true')
+      is_expected.to contain_oslo__log('{{cookiecutter.project_name}}_config').with(
+        :use_syslog          => true,
+        :use_stderr          => false,
+        :syslog_log_facility => 'LOG_FOO',
+        :log_dir             => '/var/log',
+        :log_file            => '/var/tmp/{{cookiecutter.project_name}}_random.log',
+        :debug               => true,
+      )
     end
   end
 
   shared_examples_for 'logging params set' do
     it 'enables logging params' do
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/logging_context_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/logging_default_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/logging_debug_format_suffix').with_value(
-        '%(funcName)s %(pathname)s:%(lineno)d')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/logging_exception_prefix').with_value(
-       '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_config_append').with_value(
-        '/etc/{{cookiecutter.project_name}}/logging.conf')
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/publish_errors').with_value(
-        true)
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/default_log_levels').with_value(
-        'amqp=WARN,amqplib=WARN,boto=WARN,iso8601=WARN,qpid=WARN,requests.packages.urllib3.connectionpool=WARN,sqlalchemy=WARN,suds=INFO')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/fatal_deprecations').with_value(
-        true)
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/instance_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/instance_uuid_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_{{cookiecutter.project_name}}_config('DEFAULT/log_date_format').with_value(
-        '%Y-%m-%d %H:%M:%S')
+      is_expected.to contain_oslo__log('{{cookiecutter.project_name}}_config').with(
+        :logging_context_format_string =>
+          '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s',
+        :logging_default_format_string => '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s',
+        :logging_debug_format_suffix   => '%(funcName)s %(pathname)s:%(lineno)d',
+        :logging_exception_prefix      => '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s',
+        :log_config_append             => '/etc/{{cookiecutter.project_name}}/logging.conf',
+        :publish_errors                => true,
+        :default_log_levels            => {
+          'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
+          'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
+          'requests.packages.urllib3.connectionpool' => 'WARN' },
+       :fatal_deprecations             => true,
+       :instance_format                => '[instance: %(uuid)s] ',
+       :instance_uuid_format           => '[instance: %(uuid)s] ',
+       :log_date_format                => '%Y-%m-%d %H:%M:%S',
+      )
     end
   end
-
 
   shared_examples_for 'logging params unset' do
    [ :logging_context_format_string, :logging_default_format_string,
@@ -121,7 +109,7 @@ describe '{{cookiecutter.project_name}}::logging' do
      :default_log_levels, :fatal_deprecations,
      :instance_format, :instance_uuid_format,
      :log_date_format, ].each { |param|
-        it { is_expected.to contain_{{cookiecutter.project_name}}_config("DEFAULT/#{param}").with_value('<SERVICE DEFAULT>') }
+        it { is_expected.to contain_oslo__log('{{cookiecutter.project_name}}_config').with("#{param}" => '<SERVICE DEFAULT>') }
       }
   end
 
