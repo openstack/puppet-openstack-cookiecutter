edd2a60976021f59a851d6759dd98df5da410924
ZhongShengping <chdzsp@163.com>
2018-03-22 11:09:51 +0800

    Add pool_timeout option
    Add pool_timeout option to configure this value for pool_timeout with
    SQLAlchemy.
    
    Change-Id: Id441806ab886984fbba4ebfa180f0b935fec6249
    Closes-Bug: #1757581

--- puppet-{{cookiecutter.project_name}}/manifests/db.pp
+++ puppet-{{cookiecutter.project_name}}/manifests/db.pp
@@ -38,6 +38,10 @@
 #   (Optional) If set, use this value for max_overflow with sqlalchemy.
 #   Defaults to $::os_service_default
 #
+# [*database_pool_timeout*]
+#   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
+#   Defaults to $::os_service_default
+#
 class {{cookiecutter.project_name}}::db (
   $database_connection     = 'sqlite:////var/lib/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.sqlite',
   $database_idle_timeout   = $::os_service_default,
@@ -47,6 +51,7 @@ class {{cookiecutter.project_name}}::db (
   $database_max_retries    = $::os_service_default,
   $database_retry_interval = $::os_service_default,
   $database_max_overflow   = $::os_service_default,
+  $database_pool_timeout   = $::os_service_default,
 ) {
 
   include ::{{cookiecutter.project_name}}::deps
@@ -63,5 +68,6 @@ class {{cookiecutter.project_name}}::db (
     retry_interval => $database_retry_interval,
     max_pool_size  => $database_max_pool_size,
     max_overflow   => $database_max_overflow,
+    pool_timeout   => $database_pool_timeout,
   }
 }
