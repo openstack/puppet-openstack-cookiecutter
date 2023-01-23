612c802c763f1442250902c765c12b06a4b90b21
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:57:27 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: Idcd4b7afac385757702577f7eb5e73e7a460fe86

--- puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_policy_spec.rb
+++ puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_policy_spec.rb
@@ -9,6 +9,7 @@ describe '{{cookiecutter.project_name}}::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/{{cookiecutter.project_name}}/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/{{cookiecutter.project_name}}/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe '{{cookiecutter.project_name}}::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/{{cookiecutter.project_name}}/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/{{cookiecutter.project_name}}/policy.d',
         )
       end
