7dc326a79e9de0b61ebeff11ee19729ec785198d
Takashi Kajinami <tkajinam@redhat.com>
2023-03-30 14:50:06 +0900

    Remove unnecessary fact overrides
    Change-Id: Id26d395d17496fb011cc1460f3c6a88018a4f6fb

--- puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_db_sync_spec.rb
+++ puppet-{{cookiecutter.project_name}}/spec/classes/{{cookiecutter.project_name}}_db_sync_spec.rb
@@ -29,10 +29,7 @@ describe '{{cookiecutter.project_name}}::db::sync' do
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
-        facts.merge(OSDefaults.get_facts({
-          :os_workers     => 8,
-          :concat_basedir => '/var/lib/puppet/concat'
-        }))
+        facts.merge(OSDefaults.get_facts())
       end
 
       it_behaves_like '{{cookiecutter.project_name}}-dbsync'
